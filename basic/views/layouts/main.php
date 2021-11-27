8dc73821d426636365ae434969bfc1dca34fed83
AnthonyZ <1119886232@qq.com>
2021-11-28 01:45:07 +0800

    更改了导航栏

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -10,8 +10,6 @@ use yii\bootstrap4\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
 
-$this->context->layout = false; //不使用布局
-
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
@@ -58,7 +56,17 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 
-<header>
+<header class="site-navbar py-3 js-site-navbar site-navbar-target" role="banner" id="site-navbar">
+
+  <div class="container">
+	<div class="row align-items-center">
+	  
+	  <div class="col-11 col-xl-2 site-logo">
+		<h1 class="mb-0"><a href="" class="text-white h2 mb-0">AZ</a></h1>
+	  </div>
+	  <div class="col-12 col-md-10 d-none d-xl-block">
+		<nav class="site-navigation position-relative text-right" role="navigation">
+		  <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
     <?php
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
@@ -89,6 +97,18 @@ AppAsset::register($this);
     ]);
     NavBar::end();
     ?>
+	  </ul>
+		</nav>
+	  </div>
+
+
+	  <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu h3"></span></a></div>
+
+	  </div>
+
+	</div>
+  
+  
 </header>
 
 <main role="main" class="flex-shrink-0">
